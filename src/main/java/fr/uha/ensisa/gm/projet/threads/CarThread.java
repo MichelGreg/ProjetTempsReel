f2cafb05179fe292c3acade06e78db0acc59e0a8
Greg <gregoire2@live.fr>
2023-01-15 23:54:58 +0100

    Début d'une V4 avec des virages à peu près fonctionnels

--- src/main/java/fr/uha/ensisa/gm/projet/threads/CarThread.java
+++ src/main/java/fr/uha/ensisa/gm/projet/threads/CarThread.java
@@ -12,16 +12,24 @@ public class CarThread extends Thread {
     protected static Semaphore ambulance = new Semaphore(1);
     protected final int id;
     protected Direction directionMove;
-    protected final Direction directionCar;
+    protected Direction directionCar;
+    protected final Direction turn;
     protected int x;
     protected int dx;
     protected int y;
     protected int dy;
 
     public CarThread(int id) {
-        this.directionMove = Direction.get(new Random().nextInt(0, 4));
-        this.directionCar = directionMove;
         this.id = id;
+        Random rd = new Random();
+        int direction = rd.nextInt(0, 4);
+        this.directionMove = Direction.get(direction);
+        this.directionCar = directionMove;
+        int turn;
+        do {
+            turn = rd.nextInt(4);
+        } while (turn == (direction + 1) % 4);
+        this.turn = Direction.get(turn);
     }
 
     @Override
@@ -86,16 +94,132 @@ public class CarThread extends Thread {
             System.out.printf("La voiture %d entre dans l'intersection%n", id);
             gw.moveCar(id, x, y, directionMove, directionCar, false);
             sleep(1000L);
-            x += dx;
-            y += dy;
-            gw.moveCar(id, x, y, directionMove, directionCar, false);
+//            x += dx;
+//            y += dy;
+            turn(gw);
             sleep(1000L);
             x += dx;
             y += dy;
             crossRoads.release();
             System.out.printf("La voiture %d sors de l'intersection%n", id);
         }
+    }
 
+    private void turn(GridWrapper gw) throws InterruptedException {
+        switch (directionMove) {
+            case TOP -> {
+                switch (turn) {
+                    case TOP -> {
+                        x += dx;
+                        y += dy;
+                        gw.moveCar(id, x, y, directionMove, directionCar, false);
+                        System.out.printf("La voiture %d va tout droit%n", id);
+                    }
+                    case RIGHT -> {
+                        dy = 0;
+                        dx = 1;
+                        directionCar = Direction.RIGHT;
+                        gw.moveCar(id, x, y, Direction.NONE, directionCar, false);
+                        System.out.printf("La voiture %d tourne à droite%n", id);
+                        directionMove = Direction.RIGHT;
+                    }
+                    case LEFT -> {
+                        dx = -1;
+                        dy = 0;
+                        x--;
+                        y--;
+                        directionCar = Direction.LEFT;
+                        gw.moveCar(id, x, y, Direction.TOP_LEFT, directionCar, false);
+                        System.out.printf("La voiture %d tourne à gauche%n", id);
+                        directionMove = Direction.LEFT;
+                    }
+                }
+            }
+            case RIGHT -> {
+                switch (turn) {
+                    case TOP -> {
+                        x++;
+                        y--;
+                        dx = 0;
+                        dy = -1;
+                        directionCar = Direction.TOP;
+                        gw.moveCar(id, x, y, Direction.TOP_RIGHT, directionCar, false);
+                        System.out.printf("La voiture %d tourne vers le haut%n", id);
+                        directionMove = Direction.TOP;
+                    }
+                    case RIGHT -> {
+                        x += dx;
+                        y += dy;
+                        gw.moveCar(id, x, y, directionMove, directionCar, false);
+                        System.out.printf("La voiture %d va tout droit%n", id);
+                    }
+                    case BOTTOM -> {
+                        dy = 1;
+                        dx = 0;
+                        directionCar = Direction.BOTTOM;
+                        gw.moveCar(id, x, y, Direction.NONE, directionCar, false);
+                        System.out.printf("La voiture %d tourne vers le bas%n", id);
+                        directionMove = Direction.BOTTOM;
+                    }
+                }
+            }
+            case BOTTOM -> {
+                switch (turn) {
+                    case RIGHT -> {
+                        dx = 1;
+                        dy = 0;
+                        x++;
+                        y++;
+                        directionCar = Direction.RIGHT;
+                        gw.moveCar(id, x, y, Direction.BOTTOM_RIGHT, directionCar, false);
+                        System.out.printf("La voiture %d tourne à droite%n", id);
+                        directionMove = Direction.RIGHT;
+                    }
+                    case BOTTOM -> {
+                        x += dx;
+                        y += dy;
+                        gw.moveCar(id, x, y, directionMove, directionCar, false);
+                        System.out.printf("La voiture %d va tout droit%n", id);
+                    }
+                    case LEFT -> {
+                        dy = 0;
+                        dx = -1;
+                        directionCar = Direction.LEFT;
+                        gw.moveCar(id, x, y, Direction.NONE, directionCar, false);
+                        System.out.printf("La voiture %d tourne à gauche%n", id);
+                        directionMove = Direction.LEFT;
+                    }
+                }
+            }
+            case LEFT -> {
+                switch (turn) {
+                    case TOP -> {
+                        dy = -1;
+                        dx = 0;
+                        directionCar = Direction.TOP;
+                        gw.moveCar(id, x, y, Direction.NONE, directionCar, false);
+                        System.out.printf("La voiture %d tourne vers le haut%n", id);
+                        directionMove = Direction.TOP;
+                    }
+                    case BOTTOM -> {
+                        dx = -1;
+                        dy = 0;
+                        x--;
+                        y++;
+                        directionCar = Direction.BOTTOM;
+                        gw.moveCar(id, x, y, Direction.BOTTOM_LEFT, directionCar, false);
+                        System.out.printf("La voiture %d tourne vers le bas%n", id);
+                        directionMove = Direction.BOTTOM;
+                    }
+                    case LEFT -> {
+                        x += dx;
+                        y += dy;
+                        gw.moveCar(id, x, y, directionMove, directionCar, false);
+                        System.out.printf("La voiture %d va tout droit%n", id);
+                    }
+                }
+            }
+        }
     }
     protected boolean inCrossroads (int x, int y) {
         return ((x == 4 && y == 4) || (x == 5 && y == 4) || (x == 4 && y == 5) || (x == 5 && y == 5));
